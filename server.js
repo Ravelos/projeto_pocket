dd074243f6e147ba83570ebd3570bf840560911e
raveloswaldo <raveloswaldo@gmail.com>
2021-09-24 09:19:56 -0300

    conectando a base de dados

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 const port = 3000
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb:localhost/negocioPocket', {
+    userNewUrlParser: true, userUnifyTopology:true
+})
 
 app.set("view engine", "ejs");
 
