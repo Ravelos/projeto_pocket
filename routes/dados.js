af0da5207a5fee736a6f8e1b4896683e1622bea3
raveloswaldo <raveloswaldo@gmail.com>
2021-09-29 21:55:34 -0300

    começando com as rotas

---
+++ routes/dados.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const Dados = requite("./../models/dados");
+
+const cliente = Cliente.deleteOne({_id: req.params.id}, (err) =>{
+    if(err) return res.status(400).json({
+        error: true,
+        message: "Error: cliente não foi apagado com sucesso!"
+    });
+
+    return res.json({
+        error: false,
+        message: "Cliente apagado com sucesso"
+    })
+})
