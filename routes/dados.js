2194d63bd49029b93d57d1bd882facd2b891463a
Lucas Eid <lucaseid@protonmail.com>
2021-10-04 22:38:38 -0300

    Update dados.js

--- routes/dados.js
+++ routes/dados.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Dado = require('../models/dado')
+const Dado = require('../models/dados')
 
 // Creating one
 router.post('/', async (req, res) => {
