aacb42949914bfc1d32ef7bdc9953423069b5659
Lucas Eid <lucaseid@protonmail.com>
2021-10-04 22:37:32 -0300

    Update dados.js

--- routes/dados.js
+++ routes/dados.js
@@ -1,33 +1,22 @@
-const express = require("express");
-const router = express.Router();
-const Dados = require("./../models/dados");
-const mongoose = require("mongoose")
-const dados = require("./../models/dados");
-const { Router } = require("express");
+const express = require('express')
+const router = express.Router()
+const Dado = require('../models/dado')
 
-router.post( '/', async (req, res, next) => {
-    req.dados = new Dados()
-    next()
-    let dados = new Dados({
-        nome : req.body.nome,
-        sobrenome : req.body.nome,
-        cpf : req.body.cpf,
-        endereco : req.body.endereco,
-        plano : req.body.plano,   
-    })
-    try{
-        dados = await dados.save()
-        res.redirect(`/dados/${dados.id}`)
-    } catch(e){
-        console.log(e)
-    }
-
-}  )
-
-router.delete('/:id', async (req, res) => {
-    await Article.findByIdAndDelete(req.params.id)
-    res.redirect('/')
+// Creating one
+router.post('/', async (req, res) => {
+  const dado = new Dado({
+    nome: req.body.nome,    
+    sobrenome: req.body.sobrenome,
+    cpf: req.body.cpf,
+    endereco: req.body.endereco,
+    plano: req.body.plano    
   })
-
+  try {
+    const newDado = await dado.save()
+    res.status(201).json(newDado)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
 
 module.exports = router
