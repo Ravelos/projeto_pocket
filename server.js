fbd4c849fe11b265ad923f35fb1e5a6c1c3a8ff4
raveloswaldo <raveloswaldo@gmail.com>
2021-10-06 10:50:50 -0300

    modificando server

--- server.js
+++ server.js
@@ -1,19 +1,39 @@
 const express = require('express');
 const app = express();
+const router = require("./routes/dados")
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
 
 const port = 3000
 const mongoose = require('mongoose');
 require('dotenv').config()
 mongoose.connect(process.env.MONGO, {
-    userNewUrlParser: true, userUnifyTopology:true
 })
 
 app.set("view engine", "ejs");
-
-app.get("./", (req,res) => {
+app.use(express.urlencoded({ extended: false}))
+app.get("/", (req,res) => {
     res.send("Negocio Pocket")
 } )
+mongoose.connect(process.env.MONGO, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Conectado ao Banco de Dados'))
+
+app.use("/dados", router)
+app.use(express.json())
+
+const dadosRouter = require('./routes/dados')
+app.use('/dados', dadosRouter)
+
+app.get('/', (req, res) => {
+    res.send("Negocio Pocket")
+})
 
 app.listen(port, ()=> {
     console.log(`Rodando servidor pelo localhost $(3000)`)
 });
+app.listen(3000, () => console.log('Servidor Iniciado'))
