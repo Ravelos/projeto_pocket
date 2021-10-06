d7fcb925019bcb9e5db1c356fe59402f2d57a0df
raveloswaldo <raveloswaldo@gmail.com>
2021-10-06 10:48:29 -0300

    modificando server

--- routes/dados.js
+++ routes/dados.js
@@ -1,19 +1,54 @@
 const express = require("express");
 const router = express.Router();
-const Dados = requite("./../models/dados");
+const Dados = require("./../models/dados");
+const mongoose = require("mongoose")
+const dados = require("./../models/dados");
+const { Router } = require("express");
+const express = require('express')
+const router = express.Router()
+const Dado = require('../models/dados')
 
-const cliente = Cliente.deleteOne({_id: req.params.id}, (err) =>{
-    if(err) return res.status(400).json({
-        error: true,
-        message: "Error: cliente não foi apagado com sucesso!"
-    });
-
-    return res.json({
-        error: false,
-        message: "Cliente apagado com sucesso"
+router.post( '/', async (req, res, next) => {
+    req.dados = new Dados()
+    next()
+    let dados = new Dados({
+        nome : req.body.nome,
+        sobrenome : req.body.sobrenome,
+        cpf : req.body.cpf,
+        endereco : req.body.endereco,
+        plano : req.body.plano,  
     })
-})
+    try{
+        dados = await dados.save()
+        res.redirect(`/dados/${dados.id}`)
+    } catch(e){
+        console.log(e)
+    }
 
-app.listen(port, ()=> {
-    console.log(`Rodando servidor pelo localhost $(3000)`)
-});
+}  )
+// Creating one
+router.post('/', async (req, res) => {
+  const dado = new Dado({
+    nome: req.body.nome,    
+    sobrenome: req.body.sobrenome,
+    cpf: req.body.cpf,
+    endereco: req.body.endereco,
+    plano: req.body.plano
+    
+  })
+
+router.delete('/:id', async (req, res) => {
+    await Article.findByIdAndDelete(req.params.id)
+    res.redirect('/')
+  })
+
+  })
+  try {
+    const newDado = await dado.save()
+    res.status(201).json(newDado)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  };
+
+module.exports = router
+module.exports = router
