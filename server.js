89deb2efeec55fec9b6b14f8e1e3d2941bc38eb2
Ravelos <86490406+Ravelos@users.noreply.github.com>
2021-10-14 10:42:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
-
 const dotenv = require("dotenv");
 require('dotenv').config()
 const mongoose = require('mongoose')
-const dadosRouter = require('./routes/dados')
 
 mongoose.connect(process.env.MONGO, { useNewUrlParser: true })
 const db = mongoose.connection
@@ -16,7 +14,7 @@ app.use(express.json())
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false}))
 
-
+const dadosRouter = require('./routes/dados')
 app.use('/dados', dadosRouter)
 
 app.get('/', (req, res) => {
@@ -24,42 +22,3 @@ app.get('/', (req, res) => {
 })
 
 app.listen(8080, () => console.log('Servidor Iniciado')) 
-
-const router = require("./routes/dados")
-require('dotenv').config()
-
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-
-const port = 3000
-const mongoose = require('mongoose');
-require('dotenv').config()
-mongoose.connect(process.env.MONGO, {
-})
-
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: false}))
-app.get("/", (req,res) => {
-    res.send("Negocio Pocket")
-} )
-mongoose.connect(process.env.MONGO, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Conectado ao Banco de Dados'))
-
-app.use("/dados", router)
-app.use(express.json())
-
-const dadosRouter = require('./routes/dados')
-app.use('/dados', dadosRouter)
-
-app.get('/', (req, res) => {
-    res.send("Negocio Pocket")
-})
-
-app.listen(port, ()=> {
-    console.log(`Rodando servidor pelo localhost $(3000)`)
-});
-app.listen(3000, () => console.log('Servidor Iniciado'))
-
